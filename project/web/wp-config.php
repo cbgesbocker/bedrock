ad81bdb63083640713793b1995726f8381125128
Conner <gesbocker@gmail.com>
2019-11-25 22:23:03 -0700

    Config

--- project/web/wp-config.php
+++ project/web/wp-config.php
@@ -6,4 +6,6 @@
  */
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
+
+define('WP_DEBUG', true);
 require_once ABSPATH . 'wp-settings.php';
